110f7df6466d173c65b1bd9b623b819d3156b30c
Ryan Walser <rwalser@hawaii.edu>
2015-03-05 17:11:20 -1000

    Added Variable rcv flag and clean to makefile

--- host.h
+++ host.h
@@ -15,6 +15,9 @@ typedef struct { /* State of host */
    managerLink manLink;       /* Connection to the manager */
    LinkInfo linkin;           /* Incoming communication link */
    LinkInfo linkout;          /* Outgoing communication link */
+
+   /* Added */
+   int rcvflag;
 } hostState;
 
 void hostMain(hostState * hstate);
